c0c91374119a36670de963298d4c0e158ccbb70a
Nelson Ripoll <nripoll@fbdfrozen.com>
2020-03-18 08:43:52 -0500

    add seperate GreetPage and QuotesPage folder

---
+++ Learning/Udemy/XamarinForms/GreetPage/GreetPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace XamarinForms
+{
+    public partial class GreetPage : ContentPage
+    {
+        public GreetPage()
+        {
+            InitializeComponent();
+            UpdateContent();
+        }
+
+        // UpdateContent recreates the xaml page in code-behind
+        private void UpdateContent()
+        {
+            Content = new Label
+            {
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.Center,
+                Text = "Hello, world!"
+            };
+        }
+    }
+}
