f3f7cff2df09dd08fcd0c060020e83d65dcf3050
Nelson Ripoll <nripoll@localhost.localdomain>
2020-09-24 09:45:36 -0500

    rename folder to redhat8; update script

--- redhat7/RHCSA/CH10 Virtualization/virt-install-samples/outsider1-virt-install.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-virt-install --connect qemu:///system --name=outsider1 --ram=1536 --vcpus=1 --os-type=linux --os-variant=centos8 --disk path=/var/lib/libvirt/images/outsider1-1.qcow2,size=16 --disk path=/var/lib/libvirt/images/outsider1-2.qcow2,size=2 --disk path=/var/lib/libvirt/images/outsider1-3.qcow2,size=2 --network network=outsider --location=ftp://192.168.100.1/pub/inst --extra-args="ks=ftp://192.168.100.1/pub/ks.cfg" --extra-args="repo=ftp://192.168.100.1/pub/inst" --extra-args="ip=192.168.100.150::192.168.100.1:255.255.255.0:outsider1.example.com:enp1s0:none" --extra-args="dns=192.168.100.1"
